4c251b6ddd5e4e3da4b24806dd45c85a93c75acc
Fernando Babonoyaba <fernando.babonoyaba@gmail.com>
2025-03-17 14:28:39 -0600

    fix some bugs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
+import { Route, Routes } from 'react-router'
 import './App.css'
 import Footer from './components/Footer'
 import NavBar from './components/NavBar'
 import HomePage from './pages/HomePage'
+import MeetTheHerd from './pages/MeetTheHerd'
 
 function App() {
 
   return (
     <div>
-      <NavBar/>
-      <HomePage/>
-      <Footer/>
+      {/* <NavBar/> */}
+        <Routes>
+            <Route path='/' element={<HomePage/>} />
+            <Route path="/MeetTheHurd" element={<MeetTheHerd/>} />
+
+        </Routes>
+      {/* <Footer/> */}
     </div>
   )
 }
--- frontend/src/components/RideInfoCard.tsx
+++ frontend/src/components/RideInfoCard.tsx
@@ -3,21 +3,21 @@ import { ButtonData } from "../dataRideInfo"
 
 function RideInfoCard() {
     return (
-        <div className="h-100 p-3 flex justify-between mt-4 mb-4">
+        <div className="h-200 sm:h-100 sm:p-3 sm:flex sm:justify-between mt-4 mb-4">
             <img
-                className="h-full w-18/40 object-cover rounded-l-2xl "
+                className="h-1/2 sm:h-full sm:w-18/40 object-cover  sm:rounded-l-2xl "
                 src={rideInfo.banner}
                 alt="" />
-            <div className="h-full w-22/40 flex flex-wrap">
+            <div className="h-1/2 sm:h-full sm:w-22/40 flex flex-wrap">
                 {rideInfo.buttonData.map((data: ButtonData, index: number) => {
                     let border: string = ""
                     if (index === 1) {
-                        border = "rounded-tr-2xl"
+                        border = "sm:rounded-tr-2xl"
                     } else if (index === 3) {
-                        border = "rounded-br-2xl"
+                        border = "sm:rounded-br-2xl"
                     }
                     return (
-                        <div className={`flex flex-col justify-center items-center text-center p-3 h-1/2 w-1/2 bg-cyan-800 ${border}`}>
+                        <div className={`flex flex-col justify-center items-center text-center pl-3 pr-3 h-1/2 w-1/2 bg-cyan-800 ${border}`}>
                             <img src="" alt="" />
                             <h1 className="text-2xl">{data.icon}</h1>
                             <h2 className="text-md">{data.title}</h2>

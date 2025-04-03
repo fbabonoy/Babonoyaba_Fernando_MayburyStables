553e9023129b7858914af994b46ed7e011eb62bb
Fernando Babonoyaba <fernando.babonoyaba@gmail.com>
2025-04-03 16:19:32 -0600

    updated images

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import AboutUs from "../components/AboutUs"
+import AboutUs from "../components/AboutUS"
 import CarouselRatings from "../components/CarouselRatings"
 import RideInfo from "../components/RideInfoCard"
 

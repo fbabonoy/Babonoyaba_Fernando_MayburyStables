3f75f34671c0707c07a370c006b8a956fd24a4fd
Fernando Babonoyaba <fernando.babonoyaba@gmail.com>
2025-04-12 21:53:38 -0600

    added css to resze raideinfo

--- frontend/src/components/RideInfoCard.tsx
+++ frontend/src/components/RideInfoCard.tsx
@@ -3,21 +3,21 @@ import { ButtonData } from "../Model/dataRideInfo"
 
 function RideInfoCard() {
     return (
-        <div className="h-200 md:h-150 lg:h-130 md:p-3 md:flex md:justify-between mt-4 mb-4 text-white rounded-2xl overflow-hidden m-3">
+        <div className="h-250 sm:h-200 md:h-170 lg:h-150 md:p-3 lg:flex md:justify-between mt-4 mb-4 text-white rounded-2xl overflow-hidden m-3">
             <img
-                className="h-1/2 md:h-full md:w-18/40 object-cover  md:rounded-l-2xl "
+                className="h-1/3 sm:h-1/2 lg:h-full w-full lg:w-18/40 object-cover  rounded-tl-2xl rounded-tr-2xl lg:rounded-l-2xl lg:rounded-tr-none"
                 src={rideInfo.banner}
                 alt="banner" />
-            <div className="h-1/2 md:h-full md:w-22/40 flex flex-wrap">
+            <div className="h-2/3 sm:h-1/2 w-full lg:h-full lg:w-22/40 flex sm:flex-wrap flex-col sm:flex-row rounded-b-2xl lg:rounded-b-none overflow-hidden">
                 {rideInfo.buttonData.map((data: ButtonData, index: number) => {
                     let border: string = ""
                     if (index === 1) {
-                        border = "md:rounded-tr-2xl"
+                        border = "lg:rounded-tr-2xl"
                     } else if (index === 3) {
-                        border = "md:rounded-br-2xl"
+                        border = "lg:rounded-br-2xl"
                     }
                     return (
-                        <div key={index} className={`flex justify-center items-center text-center pl-1 pr-1 md:pl-3 md:pr-3 h-1/2 w-1/2 bg-[#90260cd2] ${border}`}>
+                        <div key={index} className={`flex justify-center items-center text-center pl-1 pr-1 md:pl-3 md:pr-3 h-1/2 sm:w-1/2 bg-[#90260cd2] ${border}`}>
                             {/* <img src="" alt="" /> */}
                             <h1 className="text-4xl sm:text-3xl md:text-xl lg:text-4xl xl:text-5xl 2xl:text-7xl sm:mr-2 lg:mr-4">{data.icon}</h1>
                             <div className="text-left m-2 sm:m-0">
